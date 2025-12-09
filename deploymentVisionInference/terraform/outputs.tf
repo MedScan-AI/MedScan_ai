682f2430d63d5fbd62ab1497d5c9c154c9dde325
sriharsha.py@gmail.com <patallapalli.s@northeastern.edu>
2025-12-09 02:45:02 -0500

    fixes

--- deploymentVisionInference/terraform/outputs.tf
+++ deploymentVisionInference/terraform/outputs.tf
@@ -46,13 +46,9 @@ output "cloudbuild_service_account" {
 # Monitoring outputs
 output "monitoring_dashboard_url" {
   description = "URL to the monitoring dashboard"
-  value = var.enable_monitoring && local.monitoring_dashboard_id != "" ?
-    format(
-      "https://console.cloud.google.com/monitoring/dashboards/custom/%s?project=%s",
-      local.monitoring_dashboard_id,
-      var.project_id
-    ) :
-    "Monitoring disabled"
+  value = (var.enable_monitoring && local.monitoring_dashboard_id != "")
+    ? format("https://console.cloud.google.com/monitoring/dashboards/custom/%s?project=%s", local.monitoring_dashboard_id, var.project_id)
+    : "Monitoring disabled"
 }
 
 output "monitoring_alerts" {
